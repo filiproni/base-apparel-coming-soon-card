002dbe3db057873c35381addfa8e42fc829ded79
Roni Tandoc <ronitandoc@gmail.com>
2021-03-13 16:40:13 -0800

    Validate user email input

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Creates form
+// Creates form for email input
 const textContainer = document.querySelector(".text-container");
 const emailContainer = document.createElement("div");
 const emailForm = document.createElement("form");
@@ -9,35 +9,41 @@ emailContainer.append(emailForm);
 
 emailForm.innerHTML = `
     <input type="email" name="email" placeholder="Email Address" required>
-    <button id="email-btn" type="submit"><img src="images/icon-arrow.svg"></button>`;
+	<button id="email-btn" type="submit"><img src="images/icon-arrow.svg"></button>
+	<div id="error-msg">Please enter a valid email address.</div>
+	<div id="successful-msg">Email was submitted!</div>`;
 
 textContainer.insertBefore(emailContainer, footer);
 const emailFormButton = emailForm.querySelector("button");
+const userEmailInput = emailForm.querySelector("input");
 
-/*TODO:
-1. Error msg should DISAPPEAR once user puts in a valid email
-2. User should be able to click on field if email was invalid
-3. Msgs should only display ONCE bc you can only submit ONCE 
-4. NEED TO USE SWITCH STATEMENTS */
+// Error message disappears when user types in the input field 
+userEmailInput.addEventListener("input", (e) => {
+	e.preventDefault();
+	emailForm.classList.remove("invalid-input");
+	emailForm.querySelector("#error-msg").style.display = "none";
+});
+
+/* Validates user's input
+- checks if input field is empty
+- checks if email address is formatted correctly
+*/
+const emailValidation = () => {
+	const userEmailInputValue = emailForm.querySelector("input").value;
+	const invalidFormat = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+
+	if (
+		userEmailInputValue.length == "" ||
+		invalidFormat.test(userEmailInputValue) == false
+	) {
+		emailForm.classList.add("invalid-input");
+		emailForm.querySelector("#error-msg").style.display = "block";
+	} else {
+		emailForm.querySelector("#successful-msg").style.display = "block";
+	}
+};
+
 emailFormButton.addEventListener("click", (e) => {
 	e.preventDefault();
-	const userEmailInput = emailForm.querySelector("input").value;
-	const invalidFormat = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
-	// Checks if email input field is empty
-	// Checks if email address is formatted correctly
-	if (
-		userEmailInput.length == 0 ||
-		invalidFormat.test(userEmailInput) == false
-	) {
-		const errorMsgContainer = document.createElement("div");
-		emailForm.classList.add("invalid-input");
-		errorMsgContainer.classList.add("error-msg");
-		errorMsgContainer.innerText = "Please enter an email address.";
-		textContainer.insertBefore(errorMsgContainer, footer);
-	} else {
-		const successfulMsgContainer = document.createElement("div");
-		successfulMsgContainer.classList.add("successful-msg");
-		successfulMsgContainer.innerText = "Email is valid!";
-		textContainer.insertBefore(successfulMsgContainer, footer);
-	}
+	emailValidation();
 });
